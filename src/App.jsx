57db8f8d7872dd7c41be66c3a36b4ac1ae6b902e
Hewan <hewansolomon646@gmail.com>
2025-02-16 07:14:34 -0800

    done

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import GroupMembers from "./GroupMembers";
+
+function App() {
+  return (
+    <div>
+      <h1>Welcome to My Group</h1>
+      <GroupMembers />
+    </div>
+  );
+}
+
+export default App;
